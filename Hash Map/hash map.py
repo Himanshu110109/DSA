883bcb1cb32babd209ddee303059852457a9ba93
Sanskari coder <158292948+Himanshu110109@users.noreply.github.com>
2025-12-26 08:26:54 +0530

    Refactor Hashtable class for improved readability
    added some spaces

--- Hash Map/hash map.py
+++ Hash Map/hash map.py
@@ -1,25 +1,31 @@
 class Hashtable:
+    
     def __init__(self):
         self.max = 100
         self.arr = [None for i in range(self.max)]
         self.keys = []
+        
     def get_hash(self, key):
         hash = 0
         for char in key:
-            hash += ord(char) # ord function for getting ascii value
+            hash += ord(char)
         return hash % self.max
+        
     def __getitem__(self, key):
         h = self.get_hash(key)
         return self.arr[h]
+        
     def __setitem__(self, key, value):
         h = self.get_hash(key)
         if key not in self.keys:
             self.keys.append(key)
         self.arr[h] = value
+        
     def __delitem__(self, key):
         h = self.get_hash(key)
         self.arr[h] = None
         self.keys.remove(key)
+        
     def __str__(self):
         hashes = []
         for key in self.keys:
